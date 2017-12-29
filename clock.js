ec3a860f69b1bf6c91ddb981437d7ed765a28f98
peterpickles <lipeter2006@gmail.com>
2017-12-28 18:17:45 -0800

    adding logic to the hands

--- clock.js
+++ clock.js
@@ -3,9 +3,24 @@ var minute = document.getElementById('minute');
 var second = document.getElementById('second');
 var hour = document.getElementById('hour');
 
+//setting initial position of hands
+var secondHand = 0;
+var minuteHand = 0;
+var hourHand = 0;
 
-var updateSeconds = function () {
-			
+function showTime() {
+	secondHand += (360/60);
+	minuteHand += (360/3600);
+	hourHand += (360/43200);
+	second.style.transform = "rotate(" + secondHand + "deg)";
+	minute.style.transform = "rotate(" + minuteHand + "deg)";
+	hour.style.transform = "rotate(" + hourHand + "deg)";
+
+}
+
+function startClock () {
+	setInterval(showTime, 1000);
 }
 
  
+startClock();
