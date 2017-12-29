ec70ee7d76cbbe9e0f0728ee05e56a10bc7a7e4a
peterpickles <lipeter2006@gmail.com>
2017-12-28 17:57:00 -0800

    adding logic to clock hands

---
+++ clock.js
@@ -0,0 +1,11 @@
+//declare the initial level variables and retrive their element.
+var minute = document.getElementById('minute');
+var second = document.getElementById('second');
+var hour = document.getElementById('hour');
+
+
+var updateSeconds = function () {
+			
+}
+
+ 
